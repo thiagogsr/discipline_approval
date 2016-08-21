13334b7e32aa3255277d581a84308c33fb4e36f8
Thiago Guimaraes <thiagogsr@gmail.com>
2016-08-21 10:07:28 -0300

    Approvation calculator

--- DisciplineApproval/DisciplineApprovalPage.xaml.cs
+++ DisciplineApproval/DisciplineApprovalPage.xaml.cs
@@ -8,6 +8,27 @@ namespace DisciplineApproval
 		{
 			InitializeComponent();
 		}
+
+		void CheckApprovation(object sender, System.EventArgs e)
+		{
+			double firstGrade = double.Parse(firstGradeEntry.Text);
+			double lastGrade = double.Parse(lastGradeEntry.Text);
+
+			double finalAvg = (firstGrade * 0.4) + (lastGrade * 0.6);
+
+			if (finalAvg >= 6)
+			{
+				result.Text = "Passou, parabéns!";
+			}
+			else if (finalAvg >= 4)
+			{
+				result.Text = "Na trave, mas ainda pode fazer a prova final.";
+			}
+			else
+			{
+				result.Text = "Xi, não.";
+			}
+		}
 	}
 }
 
